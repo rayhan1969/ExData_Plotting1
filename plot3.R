f99f758c39a897f0bbdc31038bf2664d7e61c66f
rayhan1969 <rayhan@alumni.stanford.org>
2015-04-12 10:04:50 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,24 +1,34 @@
 #read in data file(set up working directory)
 setwd("C:/Users/ALIENWARE/Desktop/Ray/Plot")
-#formatted a certain way
-data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-#check that data is between 1/2/2007 and 2/2/2007
-#only want data between these dates
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-#str(subSetData)
-#convert Date and Time variables to Date/Time classes
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-#make sure it is numeric
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-#make sure submeter is numeric
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
-#plot dimensions
+#read in filename
+filename <- "household_power_consumption.txt"
+data <- read.table(filename, 
+header=TRUE, 
+sep=";",
+colClasses=c(“character”,”character”,
+rep(“numeric”,7)),
+na=”?”)
+#only want data between 1/2/2007 and 2/2/2007
+
+checkData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Date and Time variables converted to Date/Time classes
+datetime <- strptime(paste(checkData$Date, checkData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+#we want to worth numerics
+globalActivePower <- as.numeric(checkData$Global_active_power)
+submeters must also be numeric
+subMetering1 <- as.numeric(checkData$Sub_metering_1)
+subMetering2 <- as.numeric(checkData$Sub_metering_2)
+subMetering3 <- as.numeric(checkData$Sub_metering_3)
+#make outline
 png("plot3.png", width=480, height=480)
-#the plots
-plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="Day")
+
+plot(datetime, subMetering1, type="l", col=”black”,ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+legend(“topright”,
+col=c(“black,”red”,”blue”),
+c(“Sub_metering_1”,”Sub_metering_2”,
+“Sub_metering_3”),
+lty=1)
 dev.off()

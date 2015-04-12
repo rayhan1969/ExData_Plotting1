c481a9da304dd69c241e500d13bdc9f27cb8161e
rayhan1969 <rayhan@alumni.stanford.org>
2015-04-12 10:02:32 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,17 +1,18 @@
 #read in data file(set up working directory)
 setwd("C:/Users/ALIENWARE/Desktop/Ray/Plot")
-#formatted a certain way
-data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-#check that data is between 1/2/2007 and 2/2/2007
-#only want data between these dates
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-#str(subSetData)
+#data is read in as filename
+filename<-“ household_power_consumption.txt"
+data <- read.table(filename, header=TRUE, sep=";", colClasses=c(“character”,”character”, rep(”numeric”,7)),na=”?”)
+
+#We only want data between 1/2/2007 and 2/2/2007
+
+checkData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 #convert the Date and Time variables to Date/Time classes
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-#make sure it is numeric
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-#set plot dimensions
+datetime <- strptime(paste(checkData$Date, checkData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+#must be numeric
+globalActivePower <- as.numeric(checkData$Global_active_power)
+#specify plot
 png("plot2.png", width=480, height=480)  
-#draw plot with axes labeled
-plot(datetime, globalActivePower, type="l", xlab="Day", ylab="Global Active Power (in kilowatts)")
+#label axes and plot
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (in kilowatts)")
 dev.off()
